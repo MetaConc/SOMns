50f91ff2c4ed45642366451b0fa9c1b5861781b3
Sander <sander_l94@hotmail.com>
2017-03-22 19:24:23 +0100

    refactored to add messages class nodes now show name on hover when hidding actor both incoming and outgoing messages are hidden on click a node and th incoming message are highlighted

--- tools/kompos/src/protocol.ts
+++ tools/kompos/src/protocol.ts
@@ -1,7 +1,7 @@
 import * as d3 from "d3";
 import {Activity, IdMap} from "./messages";
 import {HistoryData} from "./history-data"
-import {dbgLog} from "./source";
+//import {dbgLog} from "./source";
 
 const actorStart = 20; // height at which actor headings are created
 const actorHeight = 30; // height of actor headings
@@ -9,10 +9,17 @@ const actorWidth = 60; // width of actor headings
 const actorSpacing = 100;
 const turnRadius = 20;
 const turnSpacing = 50; // space between consequent turns
+const turnWidth = 2;
+const turnHighlightedWidth = 5;
+const opacity = 0.5;
 
 var svgContainer;
 var color = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];
 
+//actor each have their own svg groups
+//one group for the heading: the square and the text field. 
+//one group for all the other elements: turns and messages
+//  to hide an actor the other group and all incoming messages are set to hidden
 class ActorHeading {
   name:               string;
   x:                  number;
@@ -20,7 +27,7 @@ class ActorHeading {
   color:              string;
   turnCount:          number;
   container:          d3.Selection<SVGElement>;
-  lastTurn:           turnNode;
+  turns:              TurnNode[];
   static actorCount:  number = 0;
   visibility:         boolean;  
 
@@ -31,65 +38,150 @@ class ActorHeading {
     this.y = actorStart;
     this.turnCount = 0;
     this.visibility = true;
+    this.turns = [];
+    drawActor(this);
   }
 
-  addTurn(turn: turnNode){
-    this.lastTurn = turn;
+  addTurn(turn: TurnNode){
+    this.turns.push(turn);
     return ++this.turnCount;
+  }
+
+  lastTurn(){
+    return this.turns[this.turns.length - 1];
   }
 
   changeVisibility() {
     this.visibility = !this.visibility;
     if(this.visibility){
-      this.container.style("visibility", "visible");
+      this.container.style("visibility", "inherit");
     } else {
       this.container.style("visibility", "hidden");
+    }
+    for (const turn of this.turns) {
+      turn.changeVisibility(this.visibility);
     }
   }
 }
 
-class turnNode {
-  x:      number;
-  y:      number;
-  actor:  ActorHeading;
+class TurnNode {
+  x:              number;
+  y:              number;
+  incoming:       Message;
+  highlighted:    boolean;
+  actor:          ActorHeading;
+  visualization:  d3.Selection<SVGElement>;
 
-  constructor(actor: ActorHeading){
+  constructor(actor: ActorHeading, message: Message){
     var count = actor.addTurn(this);
     this.actor = actor;
     this.x = actor.x + (actorWidth / 2);
     this.y = actorStart + actorHeight + count * turnSpacing;
+    this.highlighted = false;
+    this.incoming = message; //possible no message
+    this.visualization = drawTurn(this);
+  }
+
+  highlight(){
+    this.highlighted = !this.highlighted;
+    if(this.highlighted){
+      this.visualization.style("stroke-width", turnHighlightedWidth)
+                   .style("stroke", "black");
+    } else {
+      this.visualization.style("stroke-width", turnWidth)
+                   .style("stroke", this.getColor());
+    } 
+    if(this.incoming){this.incoming.highlight(this.highlighted)};
+  }
+
+  //the turn itself is made invisible by the group, only the incoming arc needs to be explicitly made invisible
+  changeVisibility(visible: boolean){
+    if(this.incoming){this.incoming.changeVisibility(visible);}
+  }
+
+  getColor(){
+    return this.actor.color;
+  }
+
+  getText(){
+    if(this.incoming){
+      return this.incoming.getText();
+    } else {
+      return "42"
+    }
+  }
+}
+
+class Message {
+  sender:        TurnNode;
+  target:        TurnNode;
+  text:          string; 
+  visualization: d3.Selection<SVGElement>;
+
+  constructor(senderActor: ActorHeading, targetActor: ActorHeading, text: string){
+    this.text = text;
+    var lastTurn = senderActor.lastTurn();
+    if(lastTurn){
+      this.sender = lastTurn;
+    } else {
+      this.sender = new TurnNode(senderActor, null);
+    }
+    this.target = new TurnNode(targetActor, this);  
+    if(senderActor === targetActor){
+      this.visualization = drawMessageToSelf(this.sender, this.target);
+    } else {  
+      this.visualization = drawMessage(this.sender, this.target);
+    }
+  }
+
+  highlight(highlighted: boolean){
+    if(highlighted){
+      this.visualization.style("stroke-width", turnHighlightedWidth)
+                        .style("stroke", "black");
+    } else {
+      this.visualization.style("stroke-width", 1)
+                        .style("stroke", this.sender.getColor());
+    }
+  }
+
+  changeVisibility(visible: boolean){
+    if(visible){
+      this.visualization.style("visibility", "inherit");
+    } else {
+      this.visualization.style("visibility", "hidden");
+    }
+  }
+
+  getText(){
+    return this.text;
   }
 } 
-
+/*
+class EmptyMessage extends Message{
+  constructor (){};
+  highlight(_highlighted: boolean){};
+  changeVisibility(_visible: boolean){};
+}
+*/
 export class ProtocolOverview {
   private actors: IdMap<ActorHeading> = {};
   private data: HistoryData;
 
   public newActivities(newActivities: Activity[]){
     for(const act of newActivities){
-      dbgLog("actor: " + act.name);
-      if(act.type == "Actor"){
+      if(act.type === "Actor"){
         var actor = new ActorHeading(act.name);
         this.actors[act.id] = actor;
-        addActor(actor);
       }
     }
   }
 
   public newMessages(newMessages: [number, number, number][]){
     for(const [senderId, targetId, messageId] of newMessages){
-      var sender = this.actors[senderId];
-      var target = this.actors[targetId];
-      dbgLog("message: " + this.data.getName(messageId) + " from: " + sender.name + " to: " + target.name);
-      dbgLog("target: "+targetId);
-      var lastTurn = target.lastTurn;
-      var turn = new turnNode(target);
-      addTurn(turn);
-      if(senderId == targetId){
-        addMessageToSelf(lastTurn, turn);
-      } else {
-        addMessage(sender, turn);
-      }
+      var senderActor = this.actors[senderId];
+      var targetActor = this.actors[targetId];
+      var message = this.data.getName(messageId);
+      new Message(senderActor, targetActor, message);
     }
   }
 
@@ -111,11 +203,11 @@ function displayProtocolOverview() {
     .attr("style", "background: none;");
 }
 
-function addActor(actor: ActorHeading){
+function drawActor(actor: ActorHeading){
   var actorHeading = svgContainer.append("g");
   var actorGroup = svgContainer.append("g");
   actor.container = actorGroup;
-  // adding text destroys the x coordinates of the actorHeaders
+  
   actorHeading.append("text")
     .attr("x", actor.x+actorWidth/2)
     .attr("y", actor.y+actorHeight/2)
@@ -131,41 +223,65 @@ function addActor(actor: ActorHeading){
     .attr("width", actorWidth)
     .style("fill", actor.color)
     .style("stroke", actor.color)
-    .style("opacity", 0.5)
-    .on("click", function(_d,_i){
+    .style("opacity", opacity)
+    .on("click", function(){
       actor.changeVisibility();
     });
 }
 
-function addTurn(turn: turnNode){
-  turn.actor.container.append("circle")
-    .attr("cx", turn.x)
-    .attr("cy", turn.y)
-    .attr("r", turnRadius)
-    .style("fill", "none")
-    .style("stroke", turn.actor.color);
+function drawTurn(turn: TurnNode){
+  var text = turn.actor.container.append("text")
+    .attr("x", turn.x)
+    .attr("y", turn.y)
+    .attr("font-size","20px")
+    .attr("text-anchor", "middle")
+    .style("opacity", 0)
+    .text(turn.getText());   
+
+  var visualization = turn.actor.container.append("circle")
+      .attr("cx", turn.x)
+      .attr("cy", turn.y)
+      .attr("r", turnRadius)
+      .style("fill", turn.getColor())
+      .style("opacity", opacity)
+      .style("stroke-width", turnWidth)
+      .style("stroke", turn.getColor())
+      .on("click", function(){
+        turn.highlight();
+      })
+      .on("mouseover", function(){
+        text.style("opacity", 1);
+      })
+      .on("mouseout", function(){
+        text.style("opacity", 0);
+      });
+  return visualization;
 }
 
-function addMessage(source: ActorHeading, target: turnNode){
-  source.container.append("line")
-    .attr("x1", source.lastTurn.x)
-    .attr("y1", source.lastTurn.y)
+function drawMessage(sender: TurnNode, target: TurnNode){
+  var visualization = sender.actor.container.append("line")
+    .attr("x1", sender.x)
+    .attr("y1", sender.y)
     .attr("x2", target.x)
     .attr("y2", target.y)
-    .style("stroke", source.color);
+    .style("stroke", sender.getColor());
+  return visualization;
 }
 
-const var lineFunction = d3.svg.line()
-  .x(function(d) { return d.x; })
-  .y(function(d) { return d.y; })
-  .interpolate("linear");
-
-function addMessageToSelf(source: turnNode, target: turnNode){
-  var lineData = [ { "x": source.x,   "y": source.y},
-                   { "x": source.x+turnRadius,  "y": source.y},
-                   { "x": target.x+turnRadius,  "y": target.y}, 
-                   { "x": target.x,  "y": target.y}];
-  source.actor.container.append("path")
-    .attr("d", lineFunction(lineData))
-    .attr("stroke", source.actor.color);
+function drawMessageToSelf(sender: TurnNode, target: TurnNode){
+  var lineData: [number, number][] = [
+    [ sender.x , sender.y],
+    [ sender.x+turnRadius*1.5 , sender.y],
+    [ target.x+turnRadius*1.5 , target.y], 
+    [ target.x , target.y]];
+  var lineGenerator: any = 
+      d3.svg.line()
+        .x(function(d) { return d[0]; })
+        .y(function(d) { return d[1]; })
+        .interpolate("linear");
+  var visualization = sender.actor.container.append("path")
+    .attr("d", lineGenerator(lineData))
+    .style("fill", "none")
+    .style("stroke", sender.getColor());
+  return visualization;
 }
